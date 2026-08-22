596230ed9d3bfc50dfd65e3f3a81222fdcc1b092
JEAN LUCAS ROTH <jean.roth@acad.univates>
2026-08-21 21:39:03 -0300

    primeiro commit

---
+++ 2108/App.js
@@ -0,0 +1,32 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View>
+      <View>
+        <View></View>
+        <View></View>
+      </View>
+      <View>
+        <View></View>
+        <View></View>
+        <View></View>
+      </View>
+      <View>
+        <View></View>
+        <View></View>
+        <View></View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
